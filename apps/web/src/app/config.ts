fad1fae70d79ab0eb8650cac7328cdd1014aba16
suyashshri <suyashshrivastava736@gmail.com>
2025-02-15 16:27:58 +0530

    bug fixes

--- apps/web/src/app/config.ts
+++ apps/web/src/app/config.ts
@@ -1,2 +1,2 @@
-export const HTTP_BACKEND = "http://http-sketchflow.codexyash.com";
-export const WS_URL = "ws://ws-sketchflow.codexyash.com";
+export const HTTP_BACKEND = "https://http-sketchflow.codexyash.com";
+export const WS_URL = "wss://ws-sketchflow.codexyash.com";
